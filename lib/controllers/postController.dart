9f0b35aeb438070e5edde66cbf4fce41330be309
betse-dv <anon127nobody+dv@gmail.com>
2019-03-10 01:54:57 +0100

    Fix 'null' bug for post

--- lib/controllers/postController.dart
+++ lib/controllers/postController.dart
@@ -14,20 +14,24 @@ class PostController {
     var url = Uri.encodeFull("https://www.demivolee.com/wp-json/wp/v2/posts?slug=" + slug + "&_embed");
     http.Response res = await HttpController.get(url); 
 
-    if (res.statusCode == 200) {
-      var decodedJson = json.decode(res.body);
-      var authorUrl = Uri.encodeFull(decodedJson[0]['_links']["author"][0]["href"].toString());
-      http.Response res2 = await HttpController.get(authorUrl); 
+    try{
+      if (res.statusCode == 200) {
+        var decodedJson = json.decode(res.body);
+        var authorUrl = Uri.encodeFull(decodedJson[0]['_links']["author"][0]["href"].toString());
+        http.Response res2 = await HttpController.get(authorUrl); 
 
-      if (res2.statusCode == 200) {
-        return Post.fromJson(decodedJson[0], Author.fromJson(json.decode(res2.body)));
+        if (res2.statusCode == 200) {
+          return Post.fromJson(decodedJson[0], Author.fromJson(json.decode(res2.body)));
+        }
+        else {
+          throw Exception('Failed to load author');
+        }
       }
       else {
-        throw Exception('Failed to load author');
+        throw Exception('Failed to load post !');
       }
-    }
-    else {
-      throw Exception('Failed to load post !');
+    } on Exception {
+      return null;
     }
   }
 
@@ -40,23 +44,27 @@ class PostController {
     /*if(res == null){
       return;
     }*/
-    
-    if (res.statusCode == 200) {
-      var resBody = json.decode(res.body);
-      if(!(resBody is Map)){
-        return List<Post>.from(resBody.map((x) => Post.fromJson(x)).toList());
-      }
-      return null;
-    }
-    else {
-      if(res.statusCode == 400){
-        print("Max Range");
+    try{
+      if (res.statusCode == 200) {
+        var resBody = json.decode(res.body);
+        if(!(resBody is Map)){
+          return List<Post>.from(resBody.map((x) => Post.fromJson(x)).toList());
+        }
         return null;
       }
-      else{
-        throw Exception('Failed to load Post');
+      else {
+        if(res.statusCode == 400){
+          print("Max Range");
+          return null;
+        }
+        else{
+          throw Exception('Failed to load Post');
+        }
       }
-  }/*
+    } on Exception {
+      return null;
+    }
+    /*
     Encoding encoding;
     var resBody = json.decode(encoding.decode(res.));
     if(!(resBody is Map)){
@@ -67,23 +75,27 @@ class PostController {
 
   static Future<String> fetchCompressedURL(link) async {
     http.Response res = await HttpController.get(Uri.encodeFull(link)); 
+    try{
       
-    if (res.statusCode == 200) {
-      var jsonSizesList = json.decode(res.body)["media_details"]["sizes"] as Map; 
-      if(jsonSizesList.containsKey("medium_large")){
-        return jsonSizesList["medium_large"]["source_url"];
+      if (res.statusCode == 200) {
+        var jsonSizesList = json.decode(res.body)["media_details"]["sizes"] as Map; 
+        if(jsonSizesList.containsKey("medium_large")){
+          return jsonSizesList["medium_large"]["source_url"];
+        }
+        else if(jsonSizesList.containsKey("mh-magazine-content")){
+          return jsonSizesList["mh-magazine-content"]["source_url"];
+        }
+        else if(jsonSizesList.containsKey("medium")){
+          return jsonSizesList["medium"]["source_url"];
+        }else{
+          return jsonSizesList["full"]["source_url"];
+        }
       }
-      else if(jsonSizesList.containsKey("mh-magazine-content")){
-        return jsonSizesList["mh-magazine-content"]["source_url"];
+      else {
+        throw Exception('Failed to load compressed media');
       }
-      else if(jsonSizesList.containsKey("medium")){
-        return jsonSizesList["medium"]["source_url"];
-      }else{
-        return jsonSizesList["full"]["source_url"];
-      }
-    }
-    else {
-      throw Exception('Failed to load compressed media');
+    } on Exception {
+      return null;
     }
   }
 }
