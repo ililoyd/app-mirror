2a05a939d22057ff5c3ea1f5ffc3c45dae76fd92
betse-dv <anon127nobody+dv@gmail.com>
2019-09-30 16:08:01 +0200

    New disqus pre-work

--- lib/page/disqus.dart
+++ lib/page/disqus.dart
@@ -2,6 +2,9 @@ import 'package:flutter/material.dart';
 //import 'package:flutter_webview_plugin/flutter_webview_plugin.dart';
 import 'dart:async';
 import 'package:webview_flutter/webview_flutter.dart';
+import 'package:flutter_webview_plugin/flutter_webview_plugin.dart';
+
+
 
 
 class DVDisqus extends StatefulWidget {
@@ -14,6 +17,13 @@ class DVDisqus extends StatefulWidget {
 
 class DVDisqusState extends State<DVDisqus> {
   final Completer<WebViewController> _controller = Completer<WebViewController>();
+  WebViewController controller;
+
+  //InAppWebViewController webView;
+    String url = "";
+    double progress = 0;
+
+  final CookieManager cookieManager = CookieManager();
 
   @override
   Widget build(BuildContext context) {
@@ -26,15 +36,68 @@ class DVDisqusState extends State<DVDisqus> {
       withZoom: true,
       withLocalStorage: true,
       hidden: true,
-      initialChild: Container(
-        color: Colors.redAccent,
-        child: const Center(
-          child: Text('Waiting.....'),
-        ),
-      ),
-    );
-*/
+      withJavascript: true,
+      supportMultipleWindows: true,
+      withOverviewMode: true,
+    
+    );*/
+    
+    var idPost = widget.disqusUri.substring(47);
+    var shortName = "lyon-sc";
+    print(widget.disqusUri);
+    print(idPost);
+    String htmlBase = "<html><head></head><body><div id='disqus_thread'></div></body>"
+        + "<meta name= \"viewport\" content=\"width=device-width, initial-scale=1\">"
+        + "<script type='text/javascript'>"
+        + "var disqus_identifier = '"
+        + idPost
+        + "';"
+        + "var disqus_shortname = '"
+        + shortName
+        + "';"
+        + " (function() { var dsq = document.createElement('script'); dsq.type = 'text/javascript'; dsq.async = true;"
+        + "dsq.src = 'https://sc-lyon.disqus.com/embed.js';"
+        + "(document.getElementsByTagName('head')[0] || document.getElementsByTagName('body')[0]).appendChild(dsq); })();"
+        + "</script></html>";
+      
+    print(htmlBase);
+    
     return Scaffold(
+      appBar: new AppBar(
+        title: const Text('Réagissez à l\'article'),
+      ),
+      body: WebView( 
+        initialUrl: new Uri.dataFromString( htmlBase, mimeType: 'text/html').toString(),
+        javascriptMode: JavascriptMode.unrestricted,
+        onWebViewCreated: (WebViewController webViewController) {
+          _controller.complete(webViewController);
+          controller = webViewController;
+        },
+        onPageFinished: (String url){
+          if (url.indexOf("disqus.com/next/login-success") > -1
+              ||
+              url.indexOf("disqus.com/_ax/facebook/complete") > -1
+              ||
+              url.indexOf("disqus.com/_ax/google/complete") > -1
+              ||
+              url.indexOf("disqus.com/_ax/twitter/complete") > -1
+              ){
+            print("\n\n\n\n\n\n");
+            print(cookieManager.getCookies());    
+            Future.delayed(const Duration(seconds: 2), (){
+              //controller.goBack();
+              controller.loadUrl(widget.disqusUri);
+              
+            });
+          }
+          
+        },
+      )
+      
+    );
+
+    
+    /*return Scaffold(
       appBar: new AppBar(
         title: const Text('Réagissez à l\'article'),
       ),
@@ -44,12 +107,96 @@ class DVDisqusState extends State<DVDisqus> {
         javascriptMode: JavascriptMode.unrestricted,
         onWebViewCreated: (WebViewController webViewController) {
           _controller.complete(webViewController);
+          controller = webViewController;
         },
-
+        onPageFinished: (String url){
+          if (url.indexOf("disqus.com/next/login-success") > -1
+              ||
+              url.indexOf("disqus.com/_ax/facebook/complete") > -1
+              ||
+              url.indexOf("disqus.com/_ax/google/complete") > -1
+              ||
+              url.indexOf("disqus.com/_ax/twitter/complete") > -1
+              ){
+            Future.delayed(const Duration(seconds: 2), (){
+              //controller.goBack();
+              controller.loadUrl(widget.disqusUri);
+              
+            });
+          }       
+        },
       ),
+    );*/
 
+    
+
+    /*
+    return Scaffold(
+      appBar: new AppBar(
+        title: const Text('Réagissez à l\'article'),
+      ),
       
-    );
+      body: InAppWebView(
+        initialUrl : new Uri.dataFromString( htmlBase, mimeType: 'text/html').toString(),
+        initialHeaders: {},
+        onWebViewCreated: (InAppWebViewController controller) {
+            print("\n\n\n\n\n\n");
+            webView = controller;
+        },
+        onLoadStart: (InAppWebViewController controller, String url) {
+          print("started $url");
+          setState(() {
+            this.url = url;
+          });
+        },
+        onLoadStop: (InAppWebViewController controller, String url) async {
+          print("stopped $url");
+        },
+        onProgressChanged:
+            (InAppWebViewController controller, int progress) {
+          setState(() {
+            this.progress = progress / 100;
+          });
+        },
+        shouldOverrideUrlLoading: (InAppWebViewController controller, String url) {
+          print("override $url");
+          controller.loadUrl(url);
+        },
+        onLoadResource: (InAppWebViewController controller, WebResourceResponse response, WebResourceRequest request) {
+          print("Started at: " +
+              response.startTime.toString() +
+              "ms ---> duration: " +
+              response.duration.toString() +
+              "ms " +
+              response.url);
+        },
+        onConsoleMessage: (InAppWebViewController controller, ConsoleMessage consoleMessage) {
+          print("""
+          console output:
+            sourceURL: ${consoleMessage.sourceURL}
+            lineNumber: ${consoleMessage.lineNumber}
+            message: ${consoleMessage.message}
+            messageLevel: ${consoleMessage.messageLevel}
+          """);
+        },
+        /*onLoadStop: (InAppWebViewController controller, String url){
+          if (url.indexOf("disqus.com/next/login-success") > -1
+              ||
+              url.indexOf("disqus.com/_ax/facebook/complete") > -1
+              ||
+              url.indexOf("disqus.com/_ax/google/complete") > -1
+              ||
+              url.indexOf("disqus.com/_ax/twitter/complete") > -1
+              ){
+            Future.delayed(const Duration(seconds: 2), (){
+              //controller.goBack();
+              controller.loadUrl(widget.disqusUri);
+              
+            });
+          }      
+        }, */
+      ),
+    );*/
 
   }
   
